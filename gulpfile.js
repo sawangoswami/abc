f664399cf490d00d53637ec94347630c0d1584d8
sawan kumar <goswamisawan759@gmail.com>
2023-01-17 00:22:48 +0530

    tried again

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,21 @@ var autoprefixer = require('gulp-autoprefixer') ;
 
 var sourcemaps = require('gulp-sourcemaps') ;
 
-var styleSRC = './src/scss/style.scss';
+var styleSRC = 'src/scss/style.scss';
 
 var styleDIST = './dist/css/';
+
+var styleWatch = 'src/scss/**/*.scss';
+
+
+
+var jsSRC = 'src/js/script.js';
+
+var jsDIST = './dist/js/';
+
+var jsWatch = 'src/js/**/*.js';
+
+
 
 gulp.task('styles', async function(){
 
@@ -35,5 +47,21 @@ gulp.task('styles', async function(){
     .pipe(sourcemaps.write('./'))
     
     .pipe(gulp.dest( styleDIST ) );
+
+});
+
+gulp.task('js', async function(){
+    gulp.src( jsSRC )
+
+    .pipe(gulp.dest( jsDIST ) )
+});
+
+gulp.task('default', gulp.parallel('styles','js'));
+
+gulp.task('watch',  function(){
+
+    gulp.watch( styleWatch, gulp.series('styles','js') );
+
+    // gulp.watch( jsWatch, [ 'js' ]  );
 
 });
